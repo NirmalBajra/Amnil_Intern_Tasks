8f6085201356b3006527e1625c629cbe0311ab55
nirmal <nirmalbajracharya99@gmail.com>
2025-01-28 18:34:13 +0545

    changes Made

--- Distinguish/obj/Debug/net9.0/Distinguish.AssemblyInfo.cs
+++ Distinguish/obj/Debug/net9.0/Distinguish.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Distinguish")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7a5ba82835bcae1bccc71b31a3c64de6e79fc48a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f645ee368215193d38b138c5479d4bbdb232147f")]
 [assembly: System.Reflection.AssemblyProductAttribute("Distinguish")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Distinguish")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
