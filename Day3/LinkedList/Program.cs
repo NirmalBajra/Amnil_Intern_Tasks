17cb768452b8cbbc9f859dc44151b16f183c0770
nirmal <nirmalbajracharya99@gmail.com>
2025-01-31 08:54:23 +0545

    Updated Linked List

--- Day3/LinkedList/Program.cs
+++ Day3/LinkedList/Program.cs
@@ -1,1 +1,56 @@
-﻿
+﻿using System;
+using System.Collections.Generic;
+
+public interface ILinkedList
+{
+    void AddNode(int value);
+    void displayList();
+}
+class LinkedListNode{
+    public int data;
+    public LinkedListNode next;
+    public LinkedListNode(int x){
+        data = x;
+        next = null;
+    }
+}
+public class LinkedList : ILinkedList
+{
+    private LinkedListNode head;
+    private int count;
+    public LinkedList(){
+        head = null;
+        count = 0;
+    }
+    public void AddNode(int data)
+    {
+        LinkedListNode node = new LinkedListNode(data);
+        node.next = head;
+        head = node;
+        count++;
+    }
+    public void displayList()
+    {
+        LinkedListNode runner = head;
+        while(runner != null)
+        {
+            Console.WriteLine(runner.data);
+            runner = runner.next;
+        }
+    }
+}
+class Program
+{
+    public static void Main(string[] args)
+    {
+        LinkedList myList = new LinkedList();
+        myList.AddNode(12);
+        myList.AddNode(22);
+        myList.AddNode(32);
+        myList.AddNode(45);
+        myList.AddNode(44);
+
+        Console.WriteLine("Linked List Data:");
+        myList.displayList();
+    }
+}
--- Day3/Stack/Program.cs
+++ Day3/Stack/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+public interface IStack
+{
+    void Push(int item);
+    void Pop();
+    void Peek();
+}
+class stack : IStack{
+    private List<int> items = new List<int>(); 
+    public void Push(int item){
+        items.Add(item);
+    }
+}
+class Program{
+
+}
